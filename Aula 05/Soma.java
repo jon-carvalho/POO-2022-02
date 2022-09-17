1c226b865293415d2636508ca7557e23078c6513
Jonatas Carvalho <jonatas@Jojo.local>
2022-09-17 11:44:10 -0300

    adding contents of classes 05

---
+++ Aula 05/Soma.java
@@ -0,0 +1,15 @@
+public class Soma {
+
+
+    public String somando(int a, int b){
+        return "Resultado: " + (a+b);
+    }
+
+    public String somando(float a, float b){
+        return "Resultado: " + (a+b);
+    }
+
+    public String somando(int a, int b, int c){
+        return "Resultado: " + (a+b+c);
+    }
+}
